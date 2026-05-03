40444ad57aaa5324e902a04cdfa05c6b9882e388
Tony Burke <117182144+anthonyjohnburke@users.noreply.github.com>
2026-05-03 12:22:07 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 
+
+
+ <script>
+    const contactLink = document.getElementById("contact-link");
+    if (contactLink) {
+      const parts = ["pintsofbangkok", "gmail", "com"];
+      const email = parts[0] + "@" + parts[1] + "." + parts[2];
+
+      contactLink.href = `mailto:${email}?subject=Guinness Bangkok Update`;
+    }
+  </script>
